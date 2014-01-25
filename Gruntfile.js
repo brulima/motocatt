7fb3e54f2975ad2915fd8c1c387d072670d55e78
Bruna Lima <bru.allima@gmail.com>
2014-01-25 17:32:18 -0200

    Criação das páginas de post

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,39 @@
 var fs = require('fs'), 
-	data = fs.readdirSync('.'), 
-	conc = {
+	conc_home = {
 		'index.html': []
-	};
-	conc['index.html'].push('_header.html');
+	}, 
+	conc_post = {};
 
-	fs.readdirSync('posts/').forEach(function (session){
-		conc['index.html'].push('posts/' + session);
+	conc_home['index.html'].push('_header.html');
+
+	fs.readdirSync('posts/').forEach(function (session){		// Para cada arquivo na pasta posts
+		conc_home['index.html'].push('posts/' + session);		// Concatena na home
+		var post = "post/"+session.replace(/[1-9]+\./, "");		// Replace nos números da data
+		conc_post[post] = [];
+		conc_post[post].push('_header.html', 'posts/' + session, '_footer.html');	//Cria o post
 	})
 
-	conc['index.html'].push('_footer.html');
-	console.log(conc);
+	conc_home['index.html'].push('_footer.html');
 
 module.exports = function(grunt) {
 	grunt.initConfig({
 		concat: {
-			all:{
-				files: conc
+			options: {
+				separator: '\n				<div class="sep"></div>\n'
+			},
+			post: {
+				files: conc_post
+			},
+			home: {
+				options: {
+					process: function(data, path){
+						path = "post/" + path.replace(/.*[1-9]+\./, "");
+						data = data.replace(/\<div class\=\"display\-block\"\>/, '<div class="display-none">');
+						data = data.replace(/\<\!\-\-ContinueLendo\-\-\>/, '<a href="' + path + '" class="keep-reading">Continue Lendo</a>');
+						return data;
+					}
+				},
+				files: conc_home
 			}
 		}
 	});
