03b2abdcfa5438d95b850df68246236b922b7936
Bruna Lima <bru.allima@gmail.com>
2014-01-25 19:48:47 -0200

    Correção do link dos posts

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,7 +55,7 @@ module.exports = function(grunt) {
 			home: {
 				options: {
 					process: function(data, path){
-						path = "post/" + path.replace(/[0-9]+\./, "");
+						path = "post/" + path.replace(/posts\/[0-9]+\./, "");
 						data = data.replace(/\<div class\=\"display\-block\"\>/, '<div class="display-none">');
 						data = data.replace(/\<\!\-\-ContinueLendo\-\-\>/, '<a href="' + path + '" class="keep-reading">Continue Lendo</a>');
 						return data;
