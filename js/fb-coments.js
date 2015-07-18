454a130a2b1eb753587c7ea3155b5720804e5824
Bruna Lima <bru.allima@gmail.com>
2015-07-17 23:53:30 -0300

    [prod] Teste facebook coments

--- js/fb-coments.js
+++ js/fb-coments.js
@@ -1,7 +1,15 @@
-(function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) return;
-js = d.createElement(s); js.id = id;
-js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.4";
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '404462066411902',
+    xfbml      : true,
+    version    : 'v2.4'
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
