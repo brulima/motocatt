c5e11bed2e4095f7a9ba4177256d5221b961fa6b
Bruna Lima <vrx_blima@uolinc.com>
2015-07-20 20:30:06 -0300

    [prod] Adicionando form de motocatts

---
+++ js/formspree.js
@@ -0,0 +1,32 @@
+(function formspree() {
+	var doc = document;
+	var getElement = function (id) {
+		return doc.getElementById(id);
+	};
+
+	getElement("name-contact-form").addEventListener("change", function() {
+		getElement("subject-contact-form").setAttribute("value", "[Contato Portfólio] " + this.value);
+	});
+
+	getElement('filePick').addEventListener('click', function() {
+		filepicker.setKey("AXGjFPz0Q4yxQ5WGd9loJz");
+		filepicker.pickMultiple(
+			{
+				mimetype: 'image/*',
+			    container: 'modal',
+			    services: ['COMPUTER', 'FACEBOOK', 'GOOGLE_DRIVE', 'INSTAGRAM', 'URL', 'WEBCAM', ],
+			    debug: true
+			},
+			function(data){
+				for (var i = 0; i < data.length; i++) {
+					var file = data[i];
+					console.log(file);
+					getElement('pics-contact-form').value += '|' + file.filename + '| ';
+				};
+			},
+			function(data){
+				console.log(JSON.stringify(error));
+			}
+		);
+	});
+})();
