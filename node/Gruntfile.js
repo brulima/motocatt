60d0157320de29272190e10ebf1a16774c852a30
Bruna Lima <vrx_blima@uolinc.com>
2015-07-21 12:05:59 -0300

    [prod] Ajustando título das páginas

--- node/Gruntfile.js
+++ node/Gruntfile.js
@@ -12,57 +12,60 @@ var headerMotocatts = '../_headerMotocatts.html';
 var indexFile = '../index.html';
 var aboutFile = '../about.html';
 var motocFile = '../motocatts.html';
+var scopedTitle = [{
+	file: 'about',
+	title: 'Sobre'
+}, {
+	file: 'contact',
+	title: 'Conte sua história'
+}, {
+	file: 'confirmation',
+	title: 'Obrigada!'
+}];
 
-var conc_home = {
-	'../index.html': [header]
+var conc_feed = {
+	'../index.html': [header],
+	'../motocatts.html': [header, headerMotocatts]
 };
 var conc_scoped = {
 	'../about.html': [header, about, footer],
 	'../contact.html': [header, contact, footer],
 	'../confirmation.html': [header, confirmation, footer]
 };
-var conc_post = {};
-var conc_author = {};
-var conc_motocatts = {
-	'../motocatts.html': [header, headerMotocatts]
-};
 
 var buildConcatObject = function () {
 	fs.readdirSync(templatePostFolder).forEach(function (session) {
 		// Concat home
-		conc_home[indexFile].push(templatePostFolder + session);
+		conc_feed[indexFile].push(templatePostFolder + session);
 
 		// Concat post page
 		var postPath = postFolder + session.replace(/[0-9]+\./, "");
 		var templatePostPath = templatePostFolder + session;
 
-		conc_post[postPath] = [];
-		conc_post[postPath].push(header, templatePostPath, fbComents, footer);
+		conc_scoped[postPath] = [];
+		conc_scoped[postPath].push(header, templatePostPath, fbComents, footer);
 
 		// Concat Author
 		var data = fs.readFileSync(templatePostPath, 'utf-8');
 		var info = data.match(/\<h2.*\<a href\=\".*author\/.*\"\>.*\<\/h2\>/)[0];
 		var auth = "../author/" + info.split('author/')[1].split('"')[0];
 
-		if (conc_author[auth]){
-			conc_author[auth].push(templatePostPath);
+		if (conc_feed[auth]){
+			conc_feed[auth].push(templatePostPath);
 		}
 		else {
-			conc_author[auth] = [header, templatePostPath];
+			conc_feed[auth] = [header, templatePostPath];
 		}
 
 		// Concat motocatt page
 		if (data.indexOf('<!--MotocattArticle-->') >= 0) {
-			conc_motocatts['../motocatts.html'].push(templatePostPath);
+			conc_feed[motocFile].push(templatePostPath);
 		}
 	});
 
-	for (var auth in conc_author && conc_author.hasOwnProperty(auth)) {
-		conc_author[auth].push(footer);
+	for (var file in conc_feed && conc_feed.hasOwnProperty(file)) {
+		conc_feed[file].push(footer);
 	}
-
-	conc_home['../index.html'].push(footer);
-	conc_motocatts['../motocatts.html'].push(footer);
 };
 
 var process = function(data, path){
@@ -85,9 +88,23 @@ var setTitle = function(dir) {
 		var data = fs.readFileSync(file, 'utf-8');
 
 		data = data.replace(/\<title\>.*\<\/title\>/, '<title>' + title + '</title>');
-
+		console.log('Title "' + title + '"');
 		fs.writeFileSync(file, data, 'utf-8');
 	});
+};
+
+var setScopedTitle = function () {
+	for (var i = 0; i < scopedTitle.length; i++) {
+		var scoped = scopedTitle[i];
+		var file = '../' + scoped.file + '.html';
+		var title = 'motocatt | ' + scoped.title;
+		var data = fs.readFileSync(file, 'utf-8');
+
+		data = data.replace(/\<title\>.*\<\/title\>/, '<title>' + title + '</title>');
+		console.log('Title "' + title + '"');
+		fs.writeFileSync(file, data, 'utf-8');
+	};
+
 };
 
 var setFacebookPage = function(dir) {
@@ -95,10 +112,12 @@ var setFacebookPage = function(dir) {
 	fs.readdirSync(dir).forEach(function (file){
 		var filePath = dir + file;
 		var data = fs.readFileSync(filePath, 'utf-8');
+		var href = 'https://brulima.github.io/motocatt/post/' + file;
 
 		data = data.replace('<div class="fb-comments" data-href="https://brulima.github.io/motocatt/post/" data-width="100%" data-numposts="3"></div>',
-			'<div class="fb-comments" data-href="https://brulima.github.io/motocatt/post/' + file + '" data-width="100%" data-numposts="3"></div>');
+			'<div class="fb-comments" data-href="' + href + '" data-width="100%" data-numposts="3"></div>');
 
+		console.log('Href "' + href + '"');
 		fs.writeFileSync(filePath, data, 'utf-8');
 	});
 };
@@ -118,26 +137,11 @@ var getTitleName = function (file) {
 module.exports = function(grunt) {
 	grunt.initConfig({
 		concat: {
-			post: {
-				files: conc_post
-			},
-			auth: {
+			feed: {
 				options: {
 					process: process
 				},
-				files: conc_author
-			},
-			mtcats: {
-				options: {
-					process: process
-				},
-				files: conc_motocatts
-			},
-			home: {
-				options: {
-					process: process
-				},
-				files: conc_home
+				files: conc_feed
 			},
 			scoped: {
 				files: conc_scoped
@@ -150,7 +154,8 @@ module.exports = function(grunt) {
 	grunt.registerTask('setTitle', setTitle);
 	grunt.registerTask('buildConcatObject', buildConcatObject);
 	grunt.registerTask('setFacebookPage', setFacebookPage);
+	grunt.registerTask('setScopedTitle', setScopedTitle);
 
-	grunt.registerTask('default', ['buildConcatObject', 'concat', 'setTitle:post', 'setTitle:author', 'setFacebookPage:post']);
+	grunt.registerTask('default', ['buildConcatObject', 'concat', 'setTitle:post', 'setTitle:author', 'setFacebookPage:post', 'setScopedTitle']);
 
 };
